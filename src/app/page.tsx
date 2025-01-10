eb323df5bd7695e90a3d2e366bbe286b2f970025
noelfoka <nomgnenoel@gmail.com>
2025-01-10 17:37:15 +0100

    bravooo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,24 @@
 import { ModeToggle } from "@/components/toggle-mode";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="w-full h-screen flex flex-col justify-center p-10">
-      <div className="justify-self-start">
+      <div className="self-end">
         <ModeToggle />
       </div>
 
-      <div className="w-full h-full flex-col justify-center items-center space-y-6"></div>
+      <div className="w-full h-full flex flex-col justify-center items-center space-y-6">
+        <h1 className="font-bold text-5xl tracking-tighter">
+          Bienvenue à la page de connexion
+        </h1>
+          <Button>
+            <Link href="/login">
+              Login
+            </Link>
+          </Button>
+      </div>
     </main>
   );
 }
