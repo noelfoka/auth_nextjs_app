beaf391261f1d11645e52f079951d10affe55c70
noelfoka <nomgnenoel@gmail.com>
2025-01-10 17:28:17 +0100

    update all

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@ import { ModeToggle } from "@/components/toggle-mode";
 export default function Home() {
   return (
     <main className="w-full h-screen flex flex-col justify-center p-10">
-      <div className="self-end">
+      <div className="justify-self-start">
         <ModeToggle />
       </div>
+
+      <div className="w-full h-full flex-col justify-center items-center space-y-6"></div>
     </main>
   );
 }
