ae38f9146a0c0beedb69611145df24328a6c5646
noelfoka <nomgnenoel@gmail.com>
2025-01-10 16:06:44 +0100

    update dbConfig

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,5 +1,15 @@
 import mongoose from 'mongoose'
 
 export async function connect() {
-  
+  try {
+    mongoose.connect(process.env.MONGO_URL!)
+    const connection = mongoose.connection;
+
+    connection.on('connected', () => {
+      console.log('MongoDB connecté avec succès');
+    })
+
+  } catch (error) {
+    console.log("Erreur de connexion à la base de données", error)
+  }
 }
