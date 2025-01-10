9a4572b5a6c60744a1dbae22fcf3e38c4e8148c0
noelfoka <nomgnenoel@gmail.com>
2025-01-10 16:13:41 +0100

    update readme

--- README.md
+++ README.md
@@ -15,3 +15,25 @@
   - dans la variable d'environnement
   avoir: mongo_url, TOKEN_SECRET, domain
   - A partir du dossier ``scr``, creer un dossier ``dbConfig`` et y mettre le fichier ``dbConfig.ts`` puis ajoute ce code:
+
+    ```ts
+    import mongoose from 'mongoose';
+    export async function connect() {
+  try {
+    mongoose.connect(process.env.MONGO_URL!)
+    const connection = mongoose.connection;
+
+    connection.on('connected', () => {
+      console.log('MongoDB connecté avec succès');
+    })
+
+    connection.on('erreur', (err) => {
+      console.log('Erreur de connexion à la base de données', err);
+      process.exit();
+    })
+
+  } catch (error) {
+    console.log("Erreur de connexion à la base de données", error)
+  }
+}
+    ```
--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,15 +1,32 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
+// Définition d'une fonction asynchrone nommée `connect` pour établir une connexion à la base de données MongoDB.
 export async function connect() {
   try {
-    mongoose.connect(process.env.MONGO_URL!)
+    // Tentative de connexion à MongoDB en utilisant l'URL spécifiée dans les variables d'environnement.
+    // `process.env.MONGO_URL` est utilisé pour récupérer l'URL de connexion depuis les variables d'environnement.
+    mongoose.connect(process.env.MONGO_URL!);
+
+    // Récupération de l'objet de connexion créé par mongoose.
     const connection = mongoose.connection;
 
+    // Ajout d'un gestionnaire d'événement pour l'événement 'connected'.
+    // Cet événement est déclenché lorsque la connexion à MongoDB est établie avec succès.
     connection.on('connected', () => {
       console.log('MongoDB connecté avec succès');
-    })
+    });
+
+    // Ajout d'un gestionnaire d'événement pour l'événement 'error'.
+    // Cet événement est déclenché en cas d'erreur lors de la tentative de connexion ou pendant une session.
+    connection.on('erreur', (err) => {
+      console.log('Erreur de connexion à la base de données', err);
+      
+      // Si une erreur survient, le processus Node.js est arrêté pour éviter un état instable.
+      process.exit();
+    });
 
   } catch (error) {
-    console.log("Erreur de connexion à la base de données", error)
+    // Gestion des erreurs au cas où la tentative de connexion initiale échoue.
+    console.log("Erreur de connexion à la base de données", error);
   }
 }
