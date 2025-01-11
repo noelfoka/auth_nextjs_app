6918684898f193ccd385462615165ba363d6c07b
noelfoka <nomgnenoel@gmail.com>
2025-01-11 06:31:23 +0100

    start login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
-import React from 'react'
+import React, { Suspense } from 'react'
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <Suspense>
+      <main className='w-full h-screen flex justify-center items-center'>
+        
+      </main>
+    </Suspense>
   )
 }
 
