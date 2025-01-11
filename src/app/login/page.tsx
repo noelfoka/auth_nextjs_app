468063a2aa8753a9e2151cb2f011a59b6d695f6d
noelfoka <nomgnenoel@gmail.com>
2025-01-11 06:43:50 +0100

    installation de react-hook-form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,21 @@
 "use client";
 
-import React, { Suspense } from 'react'
+import React, { Suspense } from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+  CardFooter,
+} from "@/components/ui/card";
 
 const LoginPage = () => {
   return (
     <Suspense>
-      <main className='w-full h-screen flex justify-center items-center'>
-        
-      </main>
+      <main className="w-full h-full flex justify-center items-center"></main>
     </Suspense>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
